6485e8149620f8bb3dd2d6b2c258975a3df4d168
Luiz Freitas <luiz_dev@outlook.com>
2024-05-03 15:40:04 -0300

    [UPD] - finish contac page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,8 +21,8 @@ const ContactPage = () => {
             <Header />
             <div className={`${styles.container} w-full max-w-5xl flex flex-col items-center`}>
                 <h1 className="mb-5 text-2xl font-bold underline">Contato</h1>
-                <div className="w-1/2 flex flex-row items-center mt-6 justify-around">
-                    <a href="https://www.linkedin.com/in/luiz-dev/" target="_blank">
+                <div className="flex flex-row items-center justify-center">
+                    <a href="https://www.linkedin.com/in/luiz-dev/" target="_blank" className="mr-5">
                         <SkillIcon icon="/icons/social/linkedin.webp" />
                     </a>
                     <a href="https://github.com/FreitasAssis" target="_blank">
@@ -32,24 +32,25 @@ const ContactPage = () => {
                 <form
                     action={process.env.FORMSPREE_ENDPOINT}
                     method="POST"
+                    className="w-1/2"
                 >
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10">
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div className="sm:col-span-4">
-                                <label htmlFor="email" className="block text-sm font-medium leading-6">
-                                    Endereço de email
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        autoComplete="email"
-                                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-900 placeholder:text-gray-900 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6 text-gray-900"
-                                        required
-                                    />
-                                </div>
+                                <div className="col-span-full">
+                                    <label htmlFor="email" className="block text-sm font-medium leading-6">
+                                        Endereço de email
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            autoComplete="email"
+                                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-900 placeholder:text-gray-900 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6 text-gray-900"
+                                            required
+                                        />
+                                    </div>
                                 </div>
 
                                 <div className="col-span-full">
@@ -73,9 +74,10 @@ const ContactPage = () => {
                     <div className="mt-10 flex items-center justify-end gap-x-6">
                         <button
                             type="submit"
-                            className="w-full flex justify-center rounded-md py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-300 hover:text-gray-900"
+                            className="w-full flex justify-center rounded-md border border-gray-300 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-300 hover:text-gray-900"
                             onMouseEnter={handleMouseEnter}
                             onMouseLeave={handleMouseLeave}
+                            onSubmit={(e) => e.preventDefault()}
                         >
                             <span className="mr-5">Enviar</span>
                             <SkillIcon icon={`${hover ? '/icons/social/email_black.svg' : '/icons/social/email.svg'}`} animation="bounce" width={20} height={20} />
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import SkillIcon from "../components/skillIcon/SkillIcon";
 import styles from './page.module.css';
 
 const ProjectsPage = () => {
@@ -8,7 +9,17 @@ const ProjectsPage = () => {
             <Header />
             <div className={`${styles.container} w-full max-w-5xl flex flex-col items-center`}>
                 <h1 className="mb-5 text-2xl font-bold underline">My projects</h1>
-                <p className="text-xl text-justify leading-relaxed">I am building this page.</p>
+                <div className="flex flex-row items-center justify-center">
+                    <a href="https://www.linkedin.com/in/luiz-dev/" target="_blank" className="mr-5">
+                        <SkillIcon icon="/icons/social/linkedin.webp" />
+                    </a>
+                    <a href="https://github.com/FreitasAssis" target="_blank">
+                        <SkillIcon icon="/icons/social/github.svg" />
+                    </a>
+                </div>
+                <div className="mt-12">
+                    <p className="text-xl text-justify leading-relaxed">I am building this page.</p>
+                </div>
             </div>
             <Footer />
         </main>
