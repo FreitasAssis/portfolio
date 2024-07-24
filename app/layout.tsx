64fa8b505baecc852b77cfa07a94677adbee2a24
Luiz Freitas <luiz_dev@outlook.com>
2024-07-24 16:03:31 -0300

    [ADD] - vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-950 text-gray-200`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
